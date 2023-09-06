346b97a54f4df8f97e32706061b1d51bcc3cf1cc
M Sreenivasulu Reddy <smukkidi@gmail.com>
2023-09-06 18:02:45 +0000

    Java Variavles

---
+++ src/main/java/com/smukkidi/javavariables/VariablesInJava.java
@@ -0,0 +1,20 @@
+package com.smukkidi.javavariables;
+
+public class VariablesInJava{
+
+    public static void main(String[] args) {
+       // Variables are containers to storing data values
+
+       // In java there are different types of variables to store data
+
+       
+      // String : String stores the text like "Sreenivasulu Reddy" String values surrounded by the double quotes. 
+      String name = "Srinivasulu Reddy";
+
+      
+      
+        
+
+    }
+    
+}
