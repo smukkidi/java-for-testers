833512fe02f9f3b21fa9cd3ae9e273ab4d4abaeb
M Sreenivasulu Reddy <smukkidi@gmail.com>
2023-09-06 04:44:10 +0000

    Hellow world program

---
+++ src/main/java/com/smukkidi/app/HelloWorldWriter.java
@@ -0,0 +1,10 @@
+package com.smukkidi.app;
+
+
+// Your First Program
+public class HelloWorldWriter{
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world.");
+    }
+}
