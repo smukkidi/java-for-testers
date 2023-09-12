b95450eff2294f1e903da884c5abba649f5cc67b
M Sreenivasulu Reddy <smukkidi@gmail.com>
2023-09-12 21:11:45 +0000

    Wtiten three new class

---
+++ src/main/java/com/javaprograms100/o002_PrintIntegersOnetoTen.java
@@ -0,0 +1,9 @@
+package com.javaprograms100;
+
+public class o002_PrintIntegersOnetoTen{
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/main/java/com/javaprograms100/o003_CommandLineArgument.java
@@ -0,0 +1,9 @@
+package com.javaprograms100;
+
+public class o003_CommandLineArgument{
+    public static void main(String[] args) {
+        for(String t : args){
+            System.out.println(t);
+        }
+    }
+}
