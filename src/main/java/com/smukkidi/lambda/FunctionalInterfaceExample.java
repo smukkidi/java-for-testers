be7ef1df4582b55f96a5c10bcb608473d4253a74
M Sreenivasulu Reddy <smukkidi@gmail.com>
2023-10-04 04:48:05 +0000

    String interview

---
+++ src/main/java/com/smukkidi/lambda/FunctionalInterfaceExample.java
@@ -0,0 +1,14 @@
+package com.smukkidi.lambda;
+
+// interface
+@FunctionalInterface // annotation
+public interface FunctionalInterfaceExample {
+    // abstract method
+    abstract void greet(String name);
+
+    // default method
+    //default int getAge(int age){
+       // return age;
+   //}
+    
+}
---
+++ src/test/java/com/smukkiditest/TestFunctionalInterface.java
@@ -0,0 +1,8 @@
+package com.smukkiditest;
+
+public class TestFunctionalInterface {
+
+   // FunctionalInterface fi = (n) -> System.out.println(n);
+  
+    
+}
