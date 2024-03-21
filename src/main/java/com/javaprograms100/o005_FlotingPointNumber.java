e112803f8e3286af566887dd88bcd6fcafed0b37
M Sreenivasulu Reddy <smukkidi@gmail.com>
2024-03-21 17:29:54 +0000

    Floting point data types

---
+++ src/main/java/com/javaprograms100/o005_FlotingPointNumber.java
@@ -0,0 +1,19 @@
+package com.javaprograms100;
+
+public class o005_FlotingPointNumber {
+    public static void main(String[] args) {
+        // float data type
+        // float: 4 bytes(32 bits)
+
+        float f = 20.00f;
+        System.out.println("Float f = " + f);
+
+        // double data type 
+        // double: 8 bytes (64 bits)
+        
+        double d = 234.00;
+        System.out.println("Double d = " + d);
+
+    }
+
+}
