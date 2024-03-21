f9573c5d9120af7125d567f5f42c321dff0e224a
M Sreenivasulu Reddy <smukkidi@gmail.com>
2024-03-21 18:00:43 +0000

    Character data type

---
+++ src/main/java/com/javaprograms100/o006_Characters.java
@@ -0,0 +1,21 @@
+package com.javaprograms100;
+
+public class o006_Characters {
+
+    public static void main(String[] args) {
+        
+        //char data type 
+        // char: 2bytes(16 bits)
+
+        char c = 'M';
+        System.out.println("C = " + c);
+
+        char s = '$';
+        System.out.println("S = " + s);
+
+        char n = '6';
+        System.out.println("N = " + n);
+
+    }
+
+}
