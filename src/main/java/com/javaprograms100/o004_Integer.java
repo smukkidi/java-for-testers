cda998c8276cefdce99ee2ee3586c724fac09a7f
M Sreenivasulu Reddy <smukkidi@gmail.com>
2024-03-21 17:07:45 +0000

    Data tupes Integer

---
+++ src/main/java/com/javaprograms100/o004_Integer.java
@@ -0,0 +1,35 @@
+// This group includes byte, short, int, long
+package com.javaprograms100;
+
+public class o004_Integer{
+
+    public static void main(String[] args) {
+        
+        // Byte data type
+        // 1 byte (8 bits)
+        // Range -127 to 128
+        // default value zero
+
+        byte b = 20;
+        System.out.println("Byte b = " + b);
+
+        //Short data type
+        // short: it is 2 bytes (16 bits)
+        
+        short s = 10;
+        System.out.println("Shrot s = " + s);
+
+        // int data type
+        // int: it is 4 bytes(32 bits)
+
+        int i = 30;
+        System.out.println("int i = " + i);
+
+        // long data type
+        // long: 8 bytes(54 bits)
+        long l = 40;
+        System.out.println("long l = " + l);
+
+    }
+
+}
